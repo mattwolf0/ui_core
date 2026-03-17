a357377d12b92235a97fe11c4173b8bc546d38bc
WMatheist <wmatheist@protonmail.com>
2026-03-17 18:11:00 +0100

    Add React entry point

---
+++ web/src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import '../assets/css/tokens.css'
+import '../assets/css/main.css'
+
+createRoot(document.getElementById('app')).render(<App />)
